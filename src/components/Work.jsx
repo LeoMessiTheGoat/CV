17e1770e825b96eb51361dc7157ba327bf1e1a60
LeoMessiTheGoat <andreiflorescu271@gmail.com>
2023-08-19 12:40:29 +0300

    dark mode

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,6 +5,7 @@ import TipCalculator from '../assets/calculator.png'
 import OnlineStore from '../assets/Online-store.png'
 import PetStore from '../assets/petstore.png'
 import WeatherApp from '../assets/weather-app.png'
+import DarkMode from '../assets/Dark-mode.png'
 
 const Work = () => {
   return (
@@ -128,6 +129,26 @@ const Work = () => {
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                   </a>
                   <a target='_blank' href="https://github.com/LeoMessiTheGoat/WeatherApp">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code </button>
+                  </a>
+                </div>
+            </div>
+          </div>
+
+          {/* Dark-mode */}
+          <div style={{backgroundImage: `url(${DarkMode})`}}
+          className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+                <span className='text-2xl font-bold text-white tracking-wider'>
+                  Weather App
+                </span>
+                <div className='pt-8 text-center'>
+                  <a target='_blank' href="https://starlit-griffin-3e7b6e.netlify.app">
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                  </a>
+                  <a target='_blank' href="https://github.com/LeoMessiTheGoat/Dark-mode">
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code </button>
                   </a>
                 </div>
